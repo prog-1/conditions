277023171281a9c5e351de7aacf02089207a474d
Sp0ozy <imolchanov210@gmail.com>
2021-09-27 20:15:20 +0300

    sorting done

---
+++ sorting/sorting.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`The program sorts three numbers.
+	Enter the numbers:`)
+	var a, b, c float64
+	fmt.Scanln(&a, &b, &c)
+	if a > b {
+		if b > c {
+			fmt.Print("Numbers in a sorted order:", c, b, a)
+			return
+		} else {
+			if b < c {
+				if a < c {
+					fmt.Print("Numbers in a sorted order:", b, a, c)
+					return
+				} else {
+					fmt.Print("Numbers in a sorted order:", b, c, a)
+					return
+				}
+
+			}
+		}
+
+	} else {
+		if a < c {
+			if b < c {
+				fmt.Print("Numbers in a sorted order:", a, b, c)
+				return
+			} else {
+				fmt.Print("Numbers in a sorted order:", a, c, b)
+				return
+			}
+		} else {
+			fmt.Print("Numbers in a sorted order:", c, a, b)
+		}
+	}
+
+}
