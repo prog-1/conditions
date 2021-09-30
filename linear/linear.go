49f018d8be8736cad389eab33c38c4c1a284b13c
Iļja Molčanovs <imolcanovs@edu.riga.lv>
2021-09-30 10:16:55 +0300

    linear bare done

---
+++ linear/linear.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The program finds solutions of a linear equation.")
+	fmt.Println("Enter the coefficients A and B:")
+	var a, b float64
+	fmt.Scan(&a, &b)
+	if a == 0 {
+		if b == 0 {
+			fmt.Println("infinite roots")
+		} else {
+			fmt.Println("no roots")
+		}
+
+	} else {
+		fmt.Println("x >", -b/a)
+	}
+}
