700dce4a0e5ee3398ecb1d4080a2e071add30830
Vladimir Satinov <vovaumni@gmail.com>
2021-10-02 14:55:19 +0300

    code for calculating area of the tirangle

---
+++ area/area.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("This program calculates areaof the triangle.")
+	fmt.Println("Enter three triangle sides: ")
+	var a, b, c float64
+	fmt.Scan(&a, &b, &c)
+	if a+b >= c {
+		if b+c >= a {
+			if c+a >= b {
+				s := (a + b + c) / 2
+				area := math.Sqrt(s * (s - a) * (s - b) * (s - c))
+				fmt.Println("Triangle area:", area)
+			} else {
+				fmt.Println("A triangle with such sides doesn't exist.")
+			}
+		} else {
+			fmt.Println("A triangle with such sides doesn't exist.")
+		}
+	} else {
+		fmt.Println("A triangle with such sides doesn't exist.")
+	}
+}
