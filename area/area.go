db9cc689a29295eca49dc3fda228787f1cb1b3cd
Vladimir Satinov <vovaumni@gmail.com>
2021-10-03 15:33:07 +0300

    code fixes

--- area/area.go
+++ area/area.go
@@ -10,18 +10,10 @@ func main() {
 	fmt.Println("Enter three triangle sides: ")
 	var a, b, c float64
 	fmt.Scan(&a, &b, &c)
-	if a+b >= c {
-		if b+c >= a {
-			if c+a >= b {
-				s := (a + b + c) / 2
-				area := math.Sqrt(s * (s - a) * (s - b) * (s - c))
-				fmt.Println("Triangle area:", area)
-			} else {
-				fmt.Println("A triangle with such sides doesn't exist.")
-			}
-		} else {
-			fmt.Println("A triangle with such sides doesn't exist.")
-		}
+	if a+b > c && b+c > a && c+a > b {
+		s := (a + b + c) / 2
+		area := math.Sqrt(s * (s - a) * (s - b) * (s - c))
+		fmt.Println("Triangle area:", area)
 	} else {
 		fmt.Println("A triangle with such sides doesn't exist.")
 	}
