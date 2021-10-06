0d988e2d843e413fa7409e368070faf975143fa6
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-06 15:10:30 +0300

    changing sorting.go

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -8,55 +8,27 @@ func main() {
 	fmt.Println("The program sorts three numbers.")
 	fmt.Println("Enter the numbers:")
 	var a, b, c float64
-	var max, min, scnd float64
 	fmt.Scan(&a, &b, &c)
-	if a != b && a != c && b != c {
-		if a > b && a > c {
-			max = a
+	if a < b && a < c {
+		if b < c {
+			fmt.Println("Numbers in a sorted order:", a, b, c)
+		} else {
+			fmt.Println("Numbers in a sorted order:", a, c, b)
 		}
-		if b > a && b > c {
-			max = b
+	} else {
+		if a < b {
+			fmt.Println("Numbers in a sorted order:", c, a, b)
+		} else {
+			if a < c {
+				fmt.Println("Numbers in a sorted order:", b, a, c)
+			} else {
+				if b < c {
+					fmt.Println("Numbers in a sorted order:", b, c, a)
+				} else {
+					fmt.Println("Numbers in a sorted order:", c, b, a)
+				}
+			}
 		}
-		if c > a && c > b {
-			max = c
-		}
-		if a < b && a < c {
-			min = a
-		}
-		if b < a && b < c {
-			min = b
-		}
-		if c < a && c < b {
-			min = c
-		}
-		scnd = a + b + c - (max + min)
+	}
 
-		fmt.Printf("Numbers in a sorted order: %v %v %v", min, scnd, max)
-	}
-	if a == b && a == c && b == c {
-		fmt.Printf("Numbers in a sorted order: %v %v %v", a, b, c)
-	}
-	if a == b || a == c || b == c {
-		if a >= b && a >= c {
-			max = a
-		}
-		if b >= a && b >= c {
-			max = b
-		}
-		if c >= a && c >= b {
-			max = c
-		}
-		if a <= b && a <= c {
-			min = a
-		}
-		if b <= a && b <= c {
-			min = b
-		}
-		if c <= a && c <= b {
-			min = c
-		}
-		scnd = a + b + c - (max + min)
-
-		fmt.Printf("Numbers in a sorted order: %v %v %v", min, scnd, max)
-	}
 }
