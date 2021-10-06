8c958b3d5add77167241a2e38dbdbafbfd728cbb
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-06 14:03:39 +0300

    changing sorting.go

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	fmt.Println("The program sorts three numbers.")
 	fmt.Println("Enter the numbers:")
 	var a, b, c float64
+	var max, min float64
 	fmt.Scan(&a, &b, &c)
-	max := math.Max(math.Max(a, b), c)
-	min := math.Min(math.Min(a, b), c)
+	if a > b && a > c {
+		max = a
+	}
+	if b > a && b > c {
+		max = b
+	} else {
+		max = c
+	}
+	if a < b && a < c {
+		min = a
+	}
+	if b < a && b < c {
+		min = b
+	} else {
+		min = c
+	}
 	scnd := a + b + c - (max + min)
 	fmt.Printf("Numbers in a sorted order: %v %v %v", min, scnd, max)
 
