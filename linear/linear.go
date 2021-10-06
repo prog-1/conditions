9343076659bdb96520e29be0e091451e84244314
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-06 13:47:54 +0300

    changing linear.go

--- linear/linear.go
+++ linear/linear.go
@@ -7,13 +7,19 @@ func main() {
 	fmt.Println("Enter A and B:")
 	var A, B float64
 	fmt.Scan(&A, &B)
-	if A != 0 {
+	if A > 0 {
 		x := -B / A
 		fmt.Printf("x > %v", x)
-	} else {
-		if B != 0 {
+	}
+	if A < 0 {
+		x1 := -B / A
+		fmt.Printf("x < %v", x1)
+	}
+	if A == 0 {
+		if B <= 0 {
 			fmt.Println("No answer.")
-		} else {
+		}
+		if B > 0 {
 			fmt.Println("Answer is all possible numbers.")
 		}
 	}
