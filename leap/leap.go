3ff25c708c36ed6a1dcb018b49d821f682344227
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-10-06 13:29:11 +0300

    changing leap.go

--- leap/leap.go
+++ leap/leap.go
@@ -7,13 +7,11 @@ func main() {
 	fmt.Println("Enter the year:")
 	var year int
 	fmt.Scan(&year)
-	if year%400 == 0 {
-		fmt.Println(year, " is a leap year.")
+	if year%400 == 0 || year%4 == 0 {
+		fmt.Println(year, "is a leap year.")
 	} else if year%100 == 0 {
-		fmt.Println(year, " is a not leap year.")
-	} else if year%4 == 0 {
-		fmt.Println(year, " is a leap year.")
+		fmt.Println(year, "is a not leap year.")
 	} else {
-		fmt.Println(year, " is a not leap year.")
+		fmt.Println(year, "is a not leap year.")
 	}
 }
