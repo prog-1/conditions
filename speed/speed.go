7cbfca60ebd3c9338d715917bfa33dd51b6296a9
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2021-10-08 13:09:08 +0300

    speed done

---
+++ speed/speed.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This program performs km/h <-> m/s speed conversion. Please enter")
+	fmt.Println("(1) if you want to convert km/h to m/s;")
+	fmt.Println("(2) if you want to convert m/s to km/h.")
+	var number float64
+	fmt.Scanln(&number)
+	if number == 1 {
+		fmt.Println("Enter the speed in km/h:")
+		var firstspeedkmh float64
+		fmt.Scanln(&firstspeedkmh)
+		firstspeedms := firstspeedkmh / 3.6
+		fmt.Println("The speed in m/s is:", firstspeedms)
+	} else if number == 2 {
+		fmt.Println("Enter the speed in m/s:")
+		var secondspeedms float64
+		fmt.Scanln(&secondspeedms)
+		secondspeedkmh := secondspeedms * 3.6
+		fmt.Println("The speed in km/h is", secondspeedkmh)
+	} else {
+		fmt.Println("Error")
+		return
+	}
+}
