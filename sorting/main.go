d1a1439b3249dd04bd405158215a66162e932bca
unknown <valerijamax@gmail.com>
2021-10-11 00:28:52 +0300

    adding corrected sorting

--- sorting/main.go
+++ sorting/main.go
@@ -9,22 +9,18 @@ func main() {
 	var a, b, c float64
 	fmt.Println("Enter three numbers:")
 	fmt.Scan(&a, &b, &c)
-	if a < b && a < c && b < c {
-		fmt.Println("Numbers in a sorted order:", a, b, c)
-	} else if a < b && a > c && b > c {
-		a, b, c := c, a, b
-		fmt.Println("Numbers in a sorted order:", a, b, c)
+	if a < b && a > c && b > c {
+		a, b, c = c, a, b
 	} else if b < a && c < a && b < c {
 		a, b, c = b, c, a
-		fmt.Println("Numbers in a sorted order:", a, b, c)
 	} else if a < c && c < b && a < b {
 		b, c = c, b
-		fmt.Println("Numbers in a sorted order:", a, b, c)
 	} else if c < b && b < a && c < a {
 		a, c = c, a
-		fmt.Println("Numbers in a sorted order:", a, b, c)
 	} else if b < a && a < c && b < c {
 		a, b = b, a
-		fmt.Println("Numbers in a sorted order:", a, b, c)
+	} else if a == b && b > c && a > c {
+		a, c = c, a
 	}
+	fmt.Println("Numbers in a sorted order:", a, b, c)
 }
