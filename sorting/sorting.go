e29f0da85a8a79a2ce7ae4db028204eebffd28cb
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2021-10-11 20:02:17 +0300

    sorting done 2

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -13,21 +13,18 @@ func main() {
 		} else {
 			a, b, c = b, c, a
 		}
-	} else if b < c && a < c {
-		if a > b {
-			a, b, c = b, a, c
-		} else {
-			a, b, c = a, b, c
-		}
-	} else if a < b && b > c {
+	} else if b > a && b > c {
 		if a > c {
 			a, b, c = c, a, b
 		} else {
 			a, b, c = a, c, b
 		}
 	} else {
-		fmt.Println("Numbers are equal.")
-		return
+		if a > b {
+			a, b, c = b, a, c
+		} else {
+			a, b, c = a, b, c
+		}
 	}
 
 	fmt.Println("Numbers in a sorted order:", a, b, c)
