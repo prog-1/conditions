da73cc5ab9536fcd39dde89b38b7b3fbdf32e5d1
unknown <valerijamax@gmail.com>
2021-10-11 19:42:39 +0300

    adding shorted area

--- speed/area.go
+++ speed/area.go
@@ -9,18 +9,10 @@ func main() {
 	var a, b, c float64
 	fmt.Println("Enter three triangle sides:")
 	fmt.Scan(&a, &b, &c)
-	if a+b > c {
-		if a+c > b {
-			if c+b > a {
-				s := (a + b + c) / 2
-				A := math.Sqrt(s * (s - a) * (s - b) * (s - c))
-				fmt.Println("Triangle area:", A)
-			} else {
-				fmt.Println("A triangle with such sides doesn't exist.")
-			}
-		} else {
-			fmt.Println("A triangle with such sides doesn't exist.")
-		}
+	if a+b > c && a+c > b && c+b > a {
+		s := (a + b + c) / 2
+		A := math.Sqrt(s * (s - a) * (s - b) * (s - c))
+		fmt.Println("Triangle area:", A)
 	} else {
 		fmt.Println("A triangle with such sides doesn't exist.")
 	}
