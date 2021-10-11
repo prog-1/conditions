ce9432a02d9027b7baf59d42d492be7b328beb7b
Alina Aniscenko <alina065b@gmail.com>
2021-10-11 19:57:28 +0300

    area.go

---
+++ area/area.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("The program determines if a triangle with such side lengths exists. If yes, calculates the area of the triangle")
+	var a, b, c float64
+	fmt.Println("Enter three triangle sides:")
+	fmt.Scan(&a, &b, &c)
+	if a+b > c && a+c > b && b+c > a {
+		semip := (a + b + c) / 2
+		area := math.Sqrt(semip * (semip - a) * (semip - b) * (semip - c))
+		fmt.Println("Triangle area:", area)
+	} else {
+		fmt.Println("A triangle with such sides doesn't exist.")
+	}
+}
