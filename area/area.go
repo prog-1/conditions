dff1aa9636e515e884abc079d9cb44f5d0565fcb
Marija Z <marija.zunda@gmail.com>
2021-10-12 19:33:11 +0300

    editing area

--- area/area.go
+++ area/area.go
@@ -10,8 +10,8 @@ func main() {
 	var a, b, c float64
 	fmt.Scanln(&a, &b, &c)
 	if a+b > c && b+c > a && c+a > b {
-		S := (a + b + c) / 2
-		Area := math.Sqrt( S * (S - a) * (S - b) * (S - c))
+		s := (a + b + c) / 2
+		area := math.Sqrt( s * (s - a) * (s - b) * (s - c))
 		fmt.Println("Triangle area:", Area)
 	} else {
 		fmt.Println("A triangle with this sides does not exist.")
